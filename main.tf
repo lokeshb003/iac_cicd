c4455d764b11e7d86f0ff51800db0c8aa3c330b6
Lokesh B <lokesh.b.2020.ad@ritchennai.edu.in>
2023-07-02 19:12:19 +0530

    Modified

--- main.tf
+++ main.tf
@@ -27,14 +27,14 @@ resource "aws_instance" "ec2_instance-1" {
   ami = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
   user_data = <<-EOL
-  #!/bin/bash
-  apt update
-  apt install openjdk-11-jdk -y
+  #!/bin/bash -xe
+
+  apt update && apt install curl -y
   curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
   echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-  apt-get update
-  apt-get install fontconfig openjdk-11-jre -y
-  apt-get install jenkins -y
+  sudo apt-get update
+  sudo apt-get install fontconfig openjdk-11-jre -y
+  sudo apt-get install jenkins -y
   systemctl status jenkins
   EOL
 
