89c1a944debb8ba29d7c199d9e427690df361776
Lokesh B <lokesh.b.2020.ad@ritchennai.edu.in>
2023-07-02 17:50:28 +0530

    Fixed Errors

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_vpc" "vpc-ec2" {
 
 resource "aws_subnet" "jenkins_subnet" {
   vpc_id = aws_vpc.vpc-ec2.id
-  cidrBlock = aws_vpc.vpc-ec2.cidr_block
+  cidr_block = aws_vpc.vpc-ec2.cidr_block
 }
 
 resource "aws_key_pair" "ssh-key-pair" {
